2a3b8d6d27dac7cea700fe5f01dc37bb231195f4
oogioogi <detacl73@gmail.com>
2022-05-02 09:32:41 +0900

    Initial Commit

---
+++ PayCalendar/PayCalendarApp.swift
@@ -0,0 +1,20 @@
+//
+//  PayCalendarApp.swift
+//  PayCalendar
+//
+//  Created by 이용석 on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct PayCalendarApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
