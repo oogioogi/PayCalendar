6e701f250b959e0efff711940d3140b6474095b1
oogioogi <detacl73@gmail.com>
2022-06-07 18:45:36 +0900

    - Main View 수정

--- PayCalendar/Views/MainView.swift
+++ PayCalendar/Views/MainView.swift
@@ -12,62 +12,57 @@ struct MainView: View {
     let persistenceController = PersistenceController.shared
     @StateObject var calendarViewModel = CalendarViewModel()
     @State var isPayCalendar: Bool = false
-    @State var backColor = Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1))
-    @State var todoBackColor = Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))
+    @State var iconTinggle: Bool = false
+    @State var isClicked: Bool = false
     
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "face.smiling.fill")
+        ZStack {
+            RadialGradient(colors: [Color("fgLiteral"), Color("bgLiteral")],
+                           center: .bottom,
+                           startRadius: 10,
+                           endRadius: UIScreen.main.bounds.height).ignoresSafeArea()
+            
+            VStack(spacing: 40) {
+                
+                topTitle
+                
+                Spacer()
+
+                if isClicked {
+                    VStack {
+                        paycalendar
+                        todolist
+                    }
+                    .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .top)))
+                }else {
+                    Text("Please Press Plus Button..")
+                        .font(.system(.headline, design: .rounded))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                }
+
+                Spacer()
+
+                Image(systemName: "plus.circle.fill")
                     .font(.largeTitle)
-                Text("Be Happy My Life")
-                    .fontWeight(.heavy)
-                    .font(.system(.largeTitle, design: .rounded))
+                    .foregroundColor(.white)
+                    .rotationEffect(Angle(degrees: isClicked ? 45 : 0))
+                    .scaleEffect(isClicked ? 1.5 : 1)
+                    .onTapGesture {
+                        withAnimation {
+                            isClicked.toggle()
+                        }
+                    }
+                
             }
-            .padding()
-            .foregroundColor(.white)
-            .background(backColor.cornerRadius(20)).opacity(0.9)
-            
-            Spacer()
-            
-            VStack(alignment: .leading, spacing: 10.0) {
-                Image(systemName: "calendar")
-                    .font(.largeTitle)
-                Text("Pay Calendar")
-                    .font(.headline)
-                Text("일일 근무 코드 기록장")
-                    .font(.subheadline)
-            }
-            .padding()
-            .foregroundColor(.white)
-            .background(backColor.cornerRadius(20))
-            .onTapGesture {
-                self.isPayCalendar.toggle()
-            }
-            
-            VStack(alignment: .leading, spacing: 10.0) {
-                Image(systemName: "text.badge.checkmark")
-                    .font(.largeTitle)
-                Text("To Do List")
-                    .font(.headline)
-                Text("일일 할일 리스트")
-                    .font(.subheadline)
-            }
-            .padding()
-            .foregroundColor(.white)
-            .background(todoBackColor.cornerRadius(20))
-            .onTapGesture {
-                self.isPayCalendar.toggle()
-            }
-            
             .fullScreenCover(isPresented: $isPayCalendar) {
                 PayCalendarView(isPayCalendar: $isPayCalendar)
                     .environment(\.managedObjectContext, persistenceController.container.viewContext)
                     .environmentObject(calendarViewModel)
             }
-            Spacer()
+            
         }
-        
+
     }
     
 }
@@ -80,41 +75,70 @@ struct MainView_Previews: PreviewProvider {
     }
 }
 
-/*
- HStack {
-     GeometryReader { inLayor in
-         HStack {
-             Spacer()
-             Image(systemName: self.isLeftArrow ? "arrow.right" : "arrow.left")
-                 .font(.system(.title, design: .default))
-                 .foregroundColor(.mint)
-                 .onTapGesture {
-                     withAnimation {
-                         self.isLeftArrow.toggle()
-                     }
-                 }
-             Image(systemName: "dollarsign.circle.fill")
-                 .font(.system(.largeTitle, design: .default))
-                 .foregroundColor(.mint)
-                 .onTapGesture {
-                     self.isMonthlyPayDetailView.toggle()
-                 }
 
-             Image(systemName: "person.circle")
-                 .font(.largeTitle)
-                 .foregroundColor(.blue)
-                 .onTapGesture {
-                     withAnimation {
-                         //  개인 설정 화면
-                         self.isInfoSetting.toggle()
-                     }
-                 }
-         }
-         .offset(self.isLeftArrow ?
-                 CGSize(width: 0, height: 0) :
-                 CGSize(width: (outLayor.size.width - inLayor.size.width) - 10, height: 0))
-     }
-
- }
+extension MainView {
+    
+    // title
+    var topTitle: some View {
+        HStack {
+            Image(systemName: "face.smiling.fill")
+                .font(.largeTitle)
+            Text("Be Happy My Life")
+                .fontWeight(.heavy)
+                .font(.system(.largeTitle, design: .rounded))
+        }
+        .padding()
+        .foregroundColor(.white)
+        .background(Color("fgLiteral").cornerRadius(20))
+        .shadow(color: .black, radius: 5, x: 2, y: 2)
+    }
+    
+    // Paycalendar View
+    var paycalendar: some View {
+        
+        VStack(alignment: .leading, spacing: 10.0) {
+            Image(systemName: "calendar")
+                .font(.largeTitle)
+                .shadow(color: .black, radius: 2, x: 2, y: 2)
+                .rotation3DEffect(Angle(degrees: iconTinggle ? 360 : 0),axis: (x: 0, y: 1, z: 0))
+            Text("Pay Calendar")
+                .font(.headline)
+            Text("일일 근무 코드 기록장")
+                .font(.subheadline)
+        }
+        .frame(width: UIScreen.main.bounds.width * 0.5)
+        .padding(.vertical, 20)
+        .foregroundColor(.white)
+        .background(Color("fgLiteral").cornerRadius(20))
+        .shadow(color: .black, radius: 5, x: 2, y: 2)
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 0.5)) {
+                iconTinggle.toggle()
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.isPayCalendar.toggle()
+            }
+        }
+    }
+    
+    // todolist View
+    var todolist: some View {
+        
+        VStack(alignment: .leading, spacing: 10.0) {
+            Image(systemName: "text.badge.checkmark")
+                .font(.largeTitle)
+            Text("To Do List")
+                .font(.headline)
+            Text("일일 할일 리스트")
+                .font(.subheadline)
+        }
+        .frame(width: UIScreen.main.bounds.width * 0.5)
+        .padding(.vertical, 20)
+        .foregroundColor(.white)
+        .background(Color("fgLiteral").cornerRadius(20))
+        .shadow(color: .black, radius: 5, x: 2, y: 2)
+        .onTapGesture {
+           //
+        }
+    }
 }
- */
